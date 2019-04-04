e8f36eb954ca65f01f2c0aa9a882028039cb3204
jeronimo <jeronimoagullo97@gmail.com>
2019-04-04 19:30:19 +0200

    changes

---
+++ MQTT-conexion/MQTT-conexion.ino
@@ -0,0 +1,14 @@
+ /* The PubSubClient library provides a client for doing simple
+ *publish/subscribe messaging with a server that supports MQTT 
+ */
+#include <PubSubClient.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
